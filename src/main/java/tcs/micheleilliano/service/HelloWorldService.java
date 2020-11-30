0df02a157edbd80ef72e842c9e94c9013b2a8d67
twhiching <mikeilliano3@gmail.com>
2020-11-30 11:52:56 -0500

    Adding project to repo

---
+++ src/main/java/tcs/micheleilliano/service/HelloWorldService.java
@@ -0,0 +1,12 @@
+package tcs.micheleilliano.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+	public String getHello() {
+		return "Hello world!";
+	}
+	
+}
