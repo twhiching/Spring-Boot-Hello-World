9ab8ee5ea42f0acfbc12463b6ab89570ac11a55f
twhiching <mikeilliano3@gmail.com>
2020-12-07 09:46:28 -0500

    Added eureka client to the class path to enable service discovery

--- src/main/java/tcs/micheleilliano/HelloWorldApplication.java
+++ src/main/java/tcs/micheleilliano/HelloWorldApplication.java
@@ -2,8 +2,10 @@ package tcs.micheleilliano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
